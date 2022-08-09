467af220748626b81f6ad291027ad54309c70e43
pradip <pselukar1415@gmail.com>
2022-08-09 23:04:23 +0530

    sonarqube

---
+++ sonarq.groovy
@@ -0,0 +1,120 @@
+pipeline {
+    agent any
+    stages {
+        stage("QA"){
+            parallel {
+                stage("QA-1"){
+                    agent any
+                    steps {
+                        echo "QA 1 PASSED"
+                    }
+                }
+
+                stage("QA-2"){
+                    agent any
+                    steps {
+                        echo "QA 2 PASSED"
+                    }
+                }
+            }
+        }
+        stage("Sonar-Scan") {
+            steps {
+                // withCredentials([string(credentialsId: 'idfs', variable: 'SONAR_TOKEN')]) {
+                sh '''
+                export PATH=$PATH:/opt/sonar-scanner/bin
+                sonar-scanner -Dsonar.login=$SONAR_TOKEN -Dsonar.projectKey=idfsbank -Dsonar.organization=atulyw
+                '''
+             }
+          }  
+        }  
+        stage("Build"){
+            steps {
+                sh '''
+                mvn clean package
+                tar -cvf $JOB_BASE_NAME-$BUILD_ID.tar **/**.war
+                '''    
+            }
+        }
+
+        stage("push-dev"){
+            when {
+                branch "develop"
+            }
+
+            steps {
+                echo "pushing artifact to s3"
+            }
+        }
+
+        stage("deploy-dev"){
+            when {
+                branch "develop"
+            }
+
+            steps {
+                echo "Deploying artifact to s3"
+            }
+        }
+
+        stage("push-test"){
+            when {
+                branch "release/*"
+            }
+
+            steps {
+                echo "pushing artifact to s3"
+            }
+        }
+
+        stage("deploy-test"){
+            when {
+                branch "release/*"
+            }
+
+            steps {
+                echo "Deploying artifact to s3"
+            }
+        }
+
+        stage("push-uat"){
+            when {
+                branch "release/*"
+            }
+
+            steps {
+                echo "pushing artifact to s3"
+            }
+        }
+
+        stage("deploy-uat"){
+            when {
+                branch "release/*"
+            }
+
+            steps {
+                echo "Deploying artifact to s3"
+            }
+        }
+
+        stage("push-prod"){
+            when {
+                branch "main"
+            }
+
+            steps {
+                echo "pushing artifact to s3"
+            }
+        }
+
+        stage("deploy-prod"){
+            when {
+                branch "main"
+            }
+
+            steps {
+                echo "Deploying artifact to s3"
+            }
+        }
+    }
+}
